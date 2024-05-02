620cf802027f276a2fb9c9e575bc9fe1bb1322f8
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2024-05-02 18:28:27 +0200

    exercise done

--- P1_2_Collections/P1_2_Collections.cpp
+++ P1_2_Collections/P1_2_Collections.cpp
@@ -1,6 +1,45 @@
 
+#include <iostream>
+#include <vector>
+#include <any>
+
+
+
+
 int main(int argc, char* argv[])
 {
+    std::vector<int> list;
+    list.push_back(137);
+    list.push_back(1000);
+    list.push_back(-200);
+
+    for (int i =0; i <list.size();i++)
+    {
+        std::cout << list[i]<<std::endl;
+    }
+
+    std::vector<std::any> arrayList;
+    arrayList.push_back("forsberg");
+    arrayList.push_back(true);
+    arrayList.push_back('a');
+    arrayList.push_back(1000);
+    arrayList.push_back(0.12f);
+
+    
+    for (const auto& item : arrayList) {
+        if (item.type() == typeid(int)) {
+            std::cout << std::any_cast<int>(item) << std::endl;
+        } else if (item.type() == typeid(const char*)) {
+            std::cout << std::any_cast<const char*>(item) << std::endl;
+        } else if (item.type() == typeid(bool)) {
+            std::cout << std::boolalpha << std::any_cast<bool>(item) << std::endl;
+        } else if (item.type() == typeid(char)) {
+            std::cout << std::any_cast<char>(item) << std::endl;
+        } else if (item.type() == typeid(float)) {
+            std::cout << std::any_cast<float>(item) << std::endl;
+        }
+        
+    }
     
     return 0;
 }
