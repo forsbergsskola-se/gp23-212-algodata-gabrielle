7155f3895e21a3a3cd74c9684639278b3cd832ff
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2024-05-07 16:07:10 +0200

    clear after copy

--- TurboCollections.Test/TurboLinkedStackTests.cpp
+++ TurboCollections.Test/TurboLinkedStackTests.cpp
@@ -3,24 +3,5 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock-matchers.h"
 
-namespace TurboCollections
-{
-    namespace TurboMathsTests
-    {
-        TEST(SayHello, WorksWithoutExeception)
-        {
-            TurboMaths::SayHello();
 
-            SUCCEED();
-  
-        }
-        TEST(GetEvenNumbersList, IteratorWorks)
-        {
-            int maxNumber=12;
-            std::vector<int> numbers = TurboMaths::GetEvenNumbersList(maxNumber);
-            EXPECT_THAT(numbers, testing::ElementsAre(0,2,4,6,8,10,12));
-        }
-    }
-    
-}
 
