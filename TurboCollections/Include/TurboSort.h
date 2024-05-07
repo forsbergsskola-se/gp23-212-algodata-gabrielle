d6a330284137134e306b0d383f21b0f2015ce562
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2024-05-07 19:15:23 +0200

    function is not working and I don't know why

---
+++ TurboCollections/Include/TurboSort.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include <vector>
+
+namespace TurboCollections
+{
+    static class TurboSort
+    {
+        TurboSort() =delete;
+    public:
+        
+        static void SelectionSort(std::vector<int> &list);
+    
+    };
+
+}
--- TurboCollections/TurboSort.cpp
+++ TurboCollections/TurboSort.cpp
@@ -1,1 +1,31 @@
 ﻿#include "TurboSort.h"
+#include <iostream>
+
+
+void TurboCollections :: TurboSort :: SelectionSort(std::vector<int>& sortNum)
+{
+    for(int i =0; i<sortNum.size()-1;i++)
+    {
+        int min = i;
+        for (int j=i+1;j<sortNum.size();j++)
+        {
+            if (sortNum[min]>sortNum[j])
+            {
+                min=j;
+            }
+        }
+        if (min!=i)
+        {
+            std::swap(sortNum[i],sortNum[min]);
+            
+        }
+    }
+   
+}
+   
+
+
+
+
+
+
