c691aabb461cb0ca0840bf141b00b419e49aa21e
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2024-05-08 13:49:36 +0200

    starting over

--- TurboCollections.Test/TurboLinkedStackTests.cpp
+++ TurboCollections.Test/TurboLinkedStackTests.cpp
@@ -1,7 +1,26 @@
-
-#include "TurboMaths.h"
+#include <gmock/internal/gmock-internal-utils.h>
+#include "../TurboCollections/TurboLinkedStack.h"
 #include "gtest/gtest.h"
 #include "gmock/gmock-matchers.h"
 
+namespace TurboCollections
+{
+    namespace TurboLinkedStackTests
+    {
+        TEST(LinkedStack, TurboLinkedStack)
+        {
+            TurboLinkedStack<int> stack;
+            
+            stack.push(10);
+            stack.push(20);
+            stack.push(30);
+            
+            assert(stack.LastNode->Value == 30);
+            assert(stack.LastNode->Previous->Value == 20);
+            assert(stack.LastNode->Previous->Previous->Value == 10);
+
+        }
+    }
+}
 
 
