62fa91c7932ca2204d53ec4c75dd05477de99453
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2024-05-29 15:00:52 +0200

    not using cpp for now. Lines commented out
    solved my linker problem

--- TurboCollections/TurboBinarySearchTree.cpp
+++ TurboCollections/TurboBinarySearchTree.cpp
@@ -1,34 +1,37 @@
 ﻿#include "TurboBinarySearchTree.h"
 
-#include "../packages/gmock.1.11.0/lib/native/include/gmock/gmock-matchers.h"
-#include "../packages/gmock.1.11.0/lib/native/include/gtest/internal/gtest-type-util.h"
+/*namespace TurboCollections {*/
 
-struct Node
-{
-    int value;
-    Node* left;
-    Node* right;
-};
-
-template <typename T>
-void TurboCollections::TurboBinarySearchTree<T>::Insert(T value)
-{
-    Node* root = new Node{value};
+    // Insert implementation
+    /*template<typename T>
+    void TurboBinarySearchTree<T>::Insert(int data)
+    {
+        Node *newNode(data);
+        if (root==nullptr)
+        {
+            return root =newNode;
+        }
+   
     
-   if (root==nullptr)
-       return;
-    Insert(root->left);
-    Insert()(root->right);
-}
+        if (data < root->value)
+        {
+        
+            Insert(root->left->value);
+        
+        }
+
+        else new Node = Insert(root->right->value);
+    }*/
+
+    
+
     //the left child has index n*2+1 
     //the right child has index n*2+2
     //(inserts a new item into the tree)
   
     
 
-    
-
-template <typename T>
+/*template <typename T>
 bool TurboCollections::TurboBinarySearchTree<T>::Search(T)
 {
     //(returns true if found)
@@ -38,6 +41,6 @@ template <typename T>
 bool TurboCollections::TurboBinarySearchTree<T>::Delete(T)
 {
     //(returns true if found and deleted),
-}
+}*/
 
 
