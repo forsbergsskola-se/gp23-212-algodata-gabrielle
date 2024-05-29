43233307aca0d4a592f1e05695f0990a7f4bbc68
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2024-05-29 15:10:46 +0200

    new test for Insert()

--- TurboCollections.Test/BinarySearchTreeTest.cpp
+++ TurboCollections.Test/BinarySearchTreeTest.cpp
@@ -1,16 +1,30 @@
+#include "TurboBinarySearchTree.h"
 #include "../TurboCollections/Include/TurboSort.h"
 #include "gtest/gtest.h"
 #include "gmock/gmock-matchers.h"
 
 namespace TurboCollections
 {
-    namespace SelectionSortTests
+    namespace BinarySearchTreeTest
     {
-        TEST(SortList, SortInRightOrder)
-        {
-            std:: vector<int> list {5,6,2,3,1,4};
-            TurboSort:: SelectionSort(list);
-            EXPECT_THAT(list, testing:: ElementsAre(1,2,3,4,5,6));
-        }
+        TurboBinarySearchTree<int> numbers;
+        
+            TEST(binary_search_tree_test, InsertMultipleValues)
+            {
+                numbers.Insert(5);
+                numbers.Insert(3);
+                numbers.Insert(7);
+
+                ASSERT_NE(numbers.root, nullptr);
+                EXPECT_EQ(numbers.root->value, 5);
+
+                ASSERT_NE(numbers.root->left, nullptr);
+                EXPECT_EQ(numbers.root->left->value, 3);
+
+                ASSERT_NE(numbers.root->right, nullptr);
+                EXPECT_EQ(numbers.root->right->value, 7);
+            }
+
+        
     }
 }
