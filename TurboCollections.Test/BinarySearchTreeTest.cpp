dff033161b34d1b1e7138322b17c4850762339f4
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2024-06-02 22:34:39 +0200

    added new bst test and have problem with numbers not getting added but replaced

--- TurboCollections.Test/BinarySearchTreeTest.cpp
+++ TurboCollections.Test/BinarySearchTreeTest.cpp
@@ -7,7 +7,7 @@ namespace TurboCollections
 {
     namespace BinarySearchTreeTest
     {
-        //TurboBinarySearchTree<int> numbers;
+        TurboBinarySearchTree<int> tree;
         
         TEST(BinarySearchTreeDelete, ReturnsTrueIfItemFound)
         {
@@ -33,10 +33,76 @@ namespace TurboCollections
             tree.Insert(3);
             tree.Insert(5);
             tree.Insert(1);
-            tree.Delete(5);
-            EXPECT_FALSE(tree.Search(5));
+            //tree.Delete(5);
+            EXPECT_TRUE(tree.Search(5));
         }
+        
+    };
+
+    TEST(BSTTest, InsertTest)
+    {
+        TurboBinarySearchTree<int> tree{};
+        EXPECT_TRUE(tree.Search(10));
+        EXPECT_TRUE(tree.Search(5));
+        EXPECT_TRUE(tree.Search(15));
+        EXPECT_TRUE(tree.Search(3));
+        EXPECT_TRUE(tree.Search(7));
+        EXPECT_TRUE(tree.Search(12));
+        EXPECT_TRUE(tree.Search(17));
+        
+        tree.Insert(6);
+        EXPECT_TRUE(tree.Search(6));
+        tree.Insert(14);
+        EXPECT_TRUE(tree.Search(14));
+    }
+
+    TEST(BSTTest, SearchTest)
+    {
+        
+        // Check existing values
+        TurboBinarySearchTree<int> tree{};
+        tree.Insert(10);
+        tree.Insert(5);
+        tree.Insert(15);
+        tree.Insert(3);
+        tree.Insert(7);
+        tree.Insert(12);
+        tree.Insert(17);
 
         
+        EXPECT_TRUE(tree.Search(10));
+        EXPECT_TRUE(tree.Search(5));
+        EXPECT_TRUE(tree.Search(15));
+        
+        EXPECT_FALSE(tree.Search(100));
+        EXPECT_FALSE(tree.Search(0));
+    }
+
+    TEST(BSTTest, RemoveTest)
+    {
+        TurboBinarySearchTree<int> tree{};
+
+        tree.Insert(10);
+        tree.Insert(5);
+        tree.Insert(15);
+        tree.Insert(3);
+        tree.Insert(7);
+        tree.Insert(12);
+        tree.Insert(17);
+
+        
+        tree.Delete(3);
+        EXPECT_FALSE(tree.Search(3));
+        
+        tree.Delete(5);
+        EXPECT_FALSE(tree.Search(5));
+        EXPECT_TRUE(tree.Search(7));
+        
+        tree.Delete(10);
+        EXPECT_FALSE(tree.Search(10));
+        EXPECT_TRUE(tree.Search(12));
+        
+        tree.Delete(100);
+        EXPECT_FALSE(tree.Search(100));
     }
 }
