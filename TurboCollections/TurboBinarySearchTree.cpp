262b5917a63a5a16c89bb50b8163710822230965
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2024-06-27 16:18:16 +0200

    no need for the cpp

--- TurboCollections/TurboBinarySearchTree.cpp
+++ TurboCollections/TurboBinarySearchTree.cpp
@@ -1,46 +1,6 @@
 ﻿#include "TurboBinarySearchTree.h"
 
-/*namespace TurboCollections {*/
 
-    // Insert implementation
-    /*template<typename T>
-    void TurboBinarySearchTree<T>::Insert(int data)
-    {
-        Node *newNode(data);
-        if (root==nullptr)
-        {
-            return root =newNode;
-        }
    
-    
-        if (data < root->value)
-        {
-        
-            Insert(root->left->value);
-        
-        }
-
-        else new Node = Insert(root->right->value);
-    }*/
-
-    
-
-    //the left child has index n*2+1 
-    //the right child has index n*2+2
-    //(inserts a new item into the tree)
-  
-    
-
-/*template <typename T>
-bool TurboCollections::TurboBinarySearchTree<T>::Search(T)
-{
-    //(returns true if found)
-}
-
-template <typename T>
-bool TurboCollections::TurboBinarySearchTree<T>::Delete(T)
-{
-    //(returns true if found and deleted),
-}*/
 
 
